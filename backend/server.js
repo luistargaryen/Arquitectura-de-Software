a409ed6c38f9129a0c1e685eef7a2703246e2b8a
luistargaryen <luis.30833554@uru.edu>
2024-12-12 19:33:35 -0400

    Aplicacion Basica

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const { Client } = require('pg');
+
+const app = express();
+const port = 5000;
+
+const client = new Client({
+  user: 'postgres',       
+  host: 'localhost',      
+  database: 'sistema_de_inscripcion',
+  password: '01092003',
+  port: '5432',       
+});
+
+client.connect();
+
+app.use(cors());
+
+app.get('/api/mensajes', async (req, res) => {
+  try {
+    const result = await client.query('SELECT nombre FROM personas');
+    const mensaje = result.rows[0].name;
+    res.send(mensaje);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error al conectar a la base de datos');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor backend corriendo en http://localhost:${port}`);
+});
